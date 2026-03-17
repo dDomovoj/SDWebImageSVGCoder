d11dca70d54294af64440bce9c0c87f87086ef66
dDomovoj <dmitryduleba@gmail.com>
2026-03-17 17:16:14 +0100

    [upd] dependency

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "git@github.com:ddomovoj/SDWebImage", branch: "master")
+        .package(url: "git@github.com:ddomovoj/SDWebImage", from: "5.21.8")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
